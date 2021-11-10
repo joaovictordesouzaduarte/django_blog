1687f1961969dc94851d43b2a9a962e9e848e56a
jvduarte <victorduarte.ufrj@gmail.com>
2021-11-09 22:40:17 -0300

    commit changes

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length = 255)
     slug = models.SlugField(max_length= 255, unique=True)
+    author = models.ForeignKey(User, on_delete = models.CASCADE)
+    body = models.TextField()
+    create = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
 
 # Create your models here.
