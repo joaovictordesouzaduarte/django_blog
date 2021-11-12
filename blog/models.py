4c8276b6d5f43163ed8ad9f249669378f9226992
joaoduarte <victorduarte.ufrj@gmail.com>
2021-11-12 07:44:47 -0300

    commit all

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.urls import reverse
 class Post(models.Model):
     title = models.CharField(max_length = 255)
     slug = models.SlugField(max_length= 255, unique=True)
@@ -9,7 +9,12 @@ class Post(models.Model):
     create = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
+    def get_absolute_url(self):
+        return reverse("blog:detail", kwargs={"slug": self.slug})
+    class Meta:
+        ordering = ("-create", )
+    
     def __str__(self):
         return self.title
+    
 
-# Create your models here.
